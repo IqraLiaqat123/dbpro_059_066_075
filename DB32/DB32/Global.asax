0d23437b979775b060229cfaa3e128b0dfba4a6d
AyeshaUmar <ayesha.idealian@gmail.com>
2019-04-20 12:58:53 +0500

    db

---
+++ DB32/DB32/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DB32.MvcApplication" Language="C#" %>
---
+++ DB32/DB32/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DB32.Startup))]
+namespace DB32
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
