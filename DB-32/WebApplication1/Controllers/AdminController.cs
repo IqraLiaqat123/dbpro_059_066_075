292755d38e992bdb79acdc282297657b9a11c1c9
Hiba Azam <hibaazam7@gmail.com>
2019-04-22 20:46:47 +0500

    dashboard to backend

--- DB-32/WebApplication1/Controllers/AdminController.cs
+++ DB-32/WebApplication1/Controllers/AdminController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class AdminController : Controller
     {
+        DB32Entities db = new DB32Entities();
         // GET: Admin
         public ActionResult Dashboard()
         {
+            ViewBag.Courses = db.Courses.SqlQuery(" SELECT * FROM dbo.Course").Count();
             return View();
         }
         public ActionResult class_section()
