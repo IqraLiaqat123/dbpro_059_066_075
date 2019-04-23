c504d3a814ce6f4b6d2c5cc16972110ecececc82
Hiba Azam <hibaazam7@gmail.com>
2019-04-23 19:19:23 +0500

    manage class

--- DB-32/WebApplication1/Controllers/AdminController.cs
+++ DB-32/WebApplication1/Controllers/AdminController.cs
@@ -10,16 +10,35 @@ namespace WebApplication1.Controllers
 {
     public class AdminController : Controller
     {
-        DB32Entities db = new DB32Entities();
+        DB32Entities2 db = new DB32Entities2();
         // GET: Admin
         public ActionResult Dashboard()
         {
-            ViewBag.Courses = db.Courses.SqlQuery(" SELECT * FROM dbo.Course").Count();
+
+
+            ViewBag.Course = db.Courses.SqlQuery("SELECT * FROM dbo.Course").Count();
+            ViewBag.Student = db.Students.SqlQuery("SELECT * FROM dbo.Student").Count();
+            ViewBag.Teacher = db.Teachers.SqlQuery("SELECT * FROM dbo.Teacher").Count();
+            ViewBag.Class = db.Classes.SqlQuery("SELECT * FROM dbo.Class").Count();
             return View();
         }
         public ActionResult class_section()
         {
             return View();
         }
-    }
+        public ActionResult createclass()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult createclass(Class c)
+        {
+            if (ModelState.IsValid) {
+                db.Classes.Add(c);
+                db.SaveChanges();
+                RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+     }
 }
--- DB-32/WebApplication1/Models/sysdiagram.cs
+++ DB-32/WebApplication1/Models/sysdiagram.cs
@@ -12,12 +12,12 @@ namespace WebApplication1.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Section
+    public partial class sysdiagram
     {
-        public int SectionID { get; set; }
         public string name { get; set; }
-        public Nullable<int> ClassID { get; set; }
-    
-        public virtual Class Class { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
