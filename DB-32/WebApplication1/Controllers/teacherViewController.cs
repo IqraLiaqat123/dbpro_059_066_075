16e95b8a9150158382d60562f08587017ae1273d
AyeshaUmar <ayesha.idealian@gmail.com>
2019-04-27 13:03:45 +0500

    Teacher Updated

---
+++ DB-32/WebApplication1/Controllers/teacherViewController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class teacherViewController : Controller
+    {
+        // GET: teacherView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Attendance()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteAttendance()
+        {
+            return View();
+        }
+
+        public ActionResult Evaluation()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteEvaluation()
+        {
+            return View();
+        }
+
+        public ActionResult Assignment()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteAssignment()
+        {
+            return View();
+        }
+
+        public ActionResult Datesheet()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteDatesheet()
+        {
+            return View();
+        }
+    }
+}
