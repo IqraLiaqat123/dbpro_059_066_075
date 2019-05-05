2cb29c407df4e3bed7d7abf134f59dbc551bfe35
Hiba Azam <hibaazam7@gmail.com>
2019-05-05 21:47:36 +0500

    student

--- DB-32/WebApplication1/Controllers/studentViewController.cs
+++ DB-32/WebApplication1/Controllers/studentViewController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class studentViewController : Controller
     {
+
+        DB32Entities1 db = new DB32Entities1();
         // GET: studentView
         public ActionResult Index()
         {
